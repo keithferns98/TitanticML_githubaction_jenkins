1bc26796ccfdf59375c59f7493494a5d49d55718
Keith Fernandes <keithfernandes311@gmail.com>
2026-03-06 11:38:43 +0530

    updated model & preprocess loader so it does not crash

--- src/services/model_service.py
+++ src/services/model_service.py
@@ -6,11 +6,25 @@ import pandas as pd
 MODEL_PATH = os.path.join("artifacts", "xgb_model.pkl")
 PREPROCESSOR_PATH = os.path.join("artifacts", "preprocessor.pkl")
 
-model = joblib.load(MODEL_PATH)
-preprocessor = joblib.load(PREPROCESSOR_PATH)
+model = None
+preprocessor = None
+
+def load_model():
+    global model
+    if model is None:
+        model = joblib.load(MODEL_PATH)
+    return model
+
+def load_preprocessor():
+    global preprocessor
+    if preprocessor is None:
+        preprocessor = joblib.load(PREPROCESSOR_PATH)
+    return preprocessor
 
 def predict(data: dict):
     df = pd.DataFrame([data])
+    model = load_model()
+    preprocessor = load_preprocessor()
     X_processed = preprocessor.transform(df)
     prediction = model.predict(X_processed)[0]
     return int(prediction)
